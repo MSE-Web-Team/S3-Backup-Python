aecd4d0e65e0908540a5810fee0a8663b0b556a7
Cody Johns <codyj6@byu.edu>
2023-11-21 10:40:24 -0700

    Add handling for multiple file extensions Change -b arg to be required

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
+#!/bin/python
 
 import boto3 
 import os
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
 import argparse
+from pathlib import Path
   
 load_dotenv()
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-f", "--file", help="specify file to upload", required=True)
-parser.add_argument("-b", "--bucket", help="specify aws bucket to upload to; otherwise use from environment variable BUCKET")
+parser.add_argument("-b", "--bucket", help="specify aws bucket to upload to", required=True)
 parser.add_argument("-k", "--keep_old", help="instruct to keep old files", action="store_true")
+parser.add_argument("-d", "--dir", help="generate a directory to store file in")
 
 args = parser.parse_args()
 
-bucket = args.bucket if args.bucket is not None else os.getenv("BUCKET")
+bucket = args.bucket
 
 session = boto3.Session(
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
@@ -26,19 +29,23 @@ s3 = session.resource('s3')
 
 date = datetime.now()
 
-filepath = args.file
-file, _, file_ext = filepath.partition('.')
-folder = date.strftime("%b-%Y")
-filename = "{}/{}_{}.{}".format(folder, file, date.strftime("%m-%d-%y"), file_ext)
+filepath = Path(args.file)
 
-s3.meta.client.upload_file( 
-    Filename=args.file, 
+folder = date.strftime("%b-%Y")
+
+filename = "{}_{}".format(date.strftime("%m-%d-%y"), filepath.name)
+
+if args.dir:
+    filename = "{}/{}_{}".format(folder, date.strftime("%m-%d-%y"), filepath.name)
+
+s3.meta.client.upload_file(
+    Filename=args.file,
     Bucket=bucket,
     Key=filename
 )
 
 if not args.keep_old:
-    expiration_threshold = (date - timedelta(days=365)).timestamp()
+    expiration_threshold = (date - timedelta(days=32)).timestamp()
     
     response = s3.meta.client.list_objects(Bucket=bucket)
 
@@ -47,7 +54,7 @@ if not args.keep_old:
     for object in response['Contents']:
         modified = object['LastModified']
         filename = object['Key']
-        print("{} {}".format(filename, modified))
+        #print("{} {}".format(filename, modified))
         if modified.timestamp() < expiration_threshold:
             removalQueue.append(filename)
 
